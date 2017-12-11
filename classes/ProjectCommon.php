f482a0fc0a575df80bc58b5c5e50c9665ae16b8d
crs2 <crs@centrum.cz>
2017-12-11 22:52:46 +0100

    ProjectCommon.php - getting rid of log_()

--- classes/ProjectCommon.php
+++ classes/ProjectCommon.php
@@ -7,35 +7,6 @@ use Psr\Log\LoggerInterface;
 class ProjectCommon {
 
     /** 
-     * @todo refactor to PSR-3 compliant logger (either BackyardError or apache/log4php 2.3.0 or ^2.3.0 ??)
-     * 
-     * log to process file
-     * @param string what to write
-     */
-    public static function log_($what) {
-        global $MyCMS;
-        if (preg_match('/^[a-z]+: /i', $what, $level)) { //@todo level není nikde nastavena?
-            $level = strtolower(trim($level[0], ': '));
-        }
-        if (!is_string($level) || !isset($MyCMS->LOG_SETTINGS[$level])) {
-            $level = 'other';
-        }
-        if (isset($MyCMS->LOG_SETTINGS[$level]['log']) && $MyCMS->LOG_SETTINGS[$level]['log'] === false) {
-            return;
-        }
-        try {
-            $logfile = fopen($MyCMS->LOG_SETTINGS[$level]['file'] ?: LOG_FILE, 'a+');
-            fwrite($logfile, $what = date('Y-m-d H:i:s ') . (string) $what . "\n");
-            fclose($logfile);
-            if (isset($MyCMS->LOG_SETTINGS[$level]['mail']) && $MyCMS->LOG_SETTINGS[$level]['mail']) {
-                mail($MyCMS->LOG_SETTINGS[$level]['mail'], 'Critical log', $what, "MIME-Version: 1.0\nContent-type:text/plain; charset=utf-8\nFrom: " . EMAIL_ADMIN . "\nDate: " . date('r'));
-            }
-        } catch (Exception $e) {
-            error_log($what);
-        }
-    }
-
-    /** 
      * Shortcut for echo'<pre>'; var_dump(); and exit;
      * @param mixed variable(s) or expression to display
      */
