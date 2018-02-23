9461ebd07ca382492ef41918a2a9e209e6e49242
Stanislav Rejthar <rejthar@gods.cz>
2018-02-23 01:08:04 +0100

    SQL and User TracyPanels also for admin.php

--- classes/MyAdmin.php
+++ classes/MyAdmin.php
@@ -2,6 +2,8 @@
 
 namespace GodsDev\MyCMS;
 
+use Tracy\Debugger;
+
 /**
  * Parent for deployed Admin instance
  * 
@@ -10,15 +12,27 @@ class MyAdmin extends MyCommon
 {
 
     /**
-     * Child may pre-populate $acceptedAttributes by addAcceptedmethod, all of them MUST be declared as protected/public variables
      * 
      * @param \GodsDev\MyCMS\MyCMS $MyCMS
      * @param array $options that overides default values within constructor
      */
     public function __construct(MyCMS $MyCMS, array $options = array())
     {
-        //nothing by default: $this->addAccepted('');
         parent::__construct($MyCMS, $options);
+    }
+
+    /**
+     * Ends Admin rendering with TracyPanels
+     */
+    public function endAdmin()
+    {
+        if (isset($_SESSION['user'])) {
+            Debugger::getBar()->addPanel(new \GodsDev\MyCMS\Tracy\BarPanelTemplate('User: ' . $_SESSION['user'], $_SESSION));
+        }
+        $sqlStatementsArray = $this->MyCMS->dbms->getStatementsArray();
+        if (!empty($sqlStatementsArray)) {
+            Debugger::getBar()->addPanel(new \GodsDev\MyCMS\Tracy\BarPanelTemplate('SQL: ' . count($sqlStatementsArray), $sqlStatementsArray));
+        }
     }
 
 }
