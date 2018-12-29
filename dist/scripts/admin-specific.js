353c98dfcc8097ee3a893cc03b29476941844d02
crs2 <crs@centrum.cz>
2018-12-29 18:32:09 +0100

    minor update on dist/*

---
+++ dist/scripts/admin-specific.js
@@ -0,0 +1,10 @@
+/**
+ * JavaScript client-side of MyCMS admin, additional project-specific code
+ * dependent JS: admin.js
+ * dependent variables: TOKEN, LISTED_FIELDS, ASSETS_SUBFOLDERS, WHERE_OPS
+ */
+
+$(document).ready(function(){
+    standardDocumentReady();
+    // insert your code here
+});
