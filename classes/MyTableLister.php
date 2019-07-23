d70bacf5450ba54f3bbee66b89c3f4bd2b858546
Stanislav Rejthar <rejthar@stanislavrejthar.com>
2019-07-23 21:53:11 +0200

    fix zobrazování tabulek s foreign key

--- classes/MyTableLister.php
+++ classes/MyTableLister.php
@@ -217,8 +217,8 @@ class MyTableLister
         foreach ($columns as $key => $value) {
             if (isset($this->fields[$key]['foreign_table']) && $this->fields[$key]['foreign_table']) {
                 $result['join'] .= ' LEFT JOIN ' . $this->fields[$key]['foreign_table']
-                    . ' ON ' . $this->table . '.' . $key
-                    . '=' . $this->fields[$key]['foreign_table'] . '.' . $this->fields[$key]['foreign_column'];
+                    . ' ON ' . '`' .$this->table . '`.`' . $key . '`'
+                    . '=' . '`' .$this->fields[$key]['foreign_table'] . '`.`' . $this->fields[$key]['foreign_column'] . '`';
                 // try if column of the same name as the table exists (as a replacement for foreign table); use the first field in the table if it doesn't exist 
                 $tmp = $this->dbms->query('SHOW FIELDS FROM ' . $this->escapeDbIdentifier($this->fields[$key]['foreign_table']))->fetch_all();
                 foreach ($tmp as $k => $v) {
@@ -229,7 +229,7 @@ class MyTableLister
                 $foreign_link = isset($tmp[$foreign_link]) && $foreign_link ? $foreign_link : reset($tmp);
                 $columns[$key] = $this->escapeDbIdentifier($this->table) . '.' . $value . ','
                         . $this->escapeDbIdentifier($this->fields[$key]['foreign_table']) . '.'
-                        . $this->escapeDbIdentifier($foreign_link) . ' AS ' . $this->escapeDbIdentifier($key . $this->DEFAULTS['FOREIGNLINK']);
+                        . $this->escapeDbIdentifier($foreign_link) . ' AS `' . $this->escapeDbIdentifier($key . $this->DEFAULTS['FOREIGNLINK']) . '`';
             }
         }
         if ($result['join']) {
