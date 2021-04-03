6a26833fbbeee6234f90687f8f42b6ae0740c029
WorkOfStan <rejthar@stanislavrejthar.com>
2021-04-03 21:45:30 +0200

    Update ThrowablePHPFunctions.php

--- classes/Throwable/ThrowablePHPFunctions.php
+++ classes/Throwable/ThrowablePHPFunctions.php
@@ -5,13 +5,12 @@ namespace GodsDev\MyCMS\Throwable;
 use Exception;
 
 
-//can't be used to return the result as mixed return would not be type strict
+//can't be used to return the result directly as mixed return would not be type strict
 //@return void
 function throwOnFalse($result){
-if ($result===false){
-throw new Exception('error '.debug_backtrace()[1]['function']);
-}
-//return $result;
+    if ($result===false){
+        throw new Exception('error '.debug_backtrace()[1]['function']);
+    }
 }
 
 function preg_match(){
