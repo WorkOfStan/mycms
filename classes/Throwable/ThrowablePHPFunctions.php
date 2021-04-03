ac66a0bbe86c3d279b734d838caa7207904dc843
WorkOfStan <rejthar@stanislavrejthar.com>
2021-04-03 21:43:55 +0200

    type strict return

--- classes/Throwable/ThrowablePHPFunctions.php
+++ classes/Throwable/ThrowablePHPFunctions.php
@@ -4,15 +4,18 @@ namespace GodsDev\MyCMS\Throwable;
 
 use Exception;
 
-//Mixed
+
+//can't be used to return the result as mixed return would not be type strict
+//@return void
 function throwOnFalse($result){
 if ($result===false){
 throw new Exception('error '.debug_backtrace()[1]['function']);
 }
-return $result;
+//return $result;
 }
 
 function preg_match(){
-  $result = preg_match();
-  return throwOnFalse($result);
+    $result = preg_match();
+    throwOnFalse($result);
+    return $result;
 }
