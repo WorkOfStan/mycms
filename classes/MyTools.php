e6cc3387b58aca08e416ef5c6af3d4270dd6025d
WorkOfStan <rejthar@stanislavrejthar.com>
2021-11-22 23:26:17 +0100

    added MyTools::h(mixed)

---
+++ classes/MyTools.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace WorkOfStan\mycms;
+
+use GodsDev\Tools\Tools;
+use Webmozart\Assert\Assert;
+
+/**
+ * Wrapper for Tools
+ *
+ * @author rejthar@stanislavrejthar.com
+ */
+class Utils
+{
+    use \Nette\SmartObject;
+
+    /**
+     * h that accepts mixed but throws exception if it isn't a string
+     *
+     * @param mixed $str
+     * @return string
+     */
+    public static function h($str)
+    {
+        Assert::string($str);
+        return Tools::h($str);
+    }
+
+}
